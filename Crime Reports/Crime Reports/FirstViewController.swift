4e8a51071994e4e5681eaba44046b62d03e69264
Chris Haen <chrishdev2007@gmail.com>
2018-03-07 10:39:53 -0800

    Project Creation

---
+++ Crime Reports/Crime Reports/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Crime Reports
+//
+//  Created by Chris Haen on 3/7/18.
+//  Copyright © 2018 Christopher Haen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

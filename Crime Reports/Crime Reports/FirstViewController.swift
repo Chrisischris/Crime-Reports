7a5bf72ca1f81704e904018c1659f82493dc2fab
Chris Haen <chrishdev2007@gmail.com>
2018-03-08 10:54:11 -0800

    Layout and Map

--- Crime Reports/Crime Reports/FirstViewController.swift
+++ Crime Reports/Crime Reports/FirstViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
-
+    
+    //Location Manager
+    let locationManager = CLLocationManager()
+    
+    //Map View Outlet
+    @IBOutlet weak var mapView: MKMapView!
+    //View Button Outlet
+    @IBOutlet weak var viewButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView?.showsUserLocation = true
+        requestLocationAccess()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //View Button Clicked
+    @IBAction func viewButtonClicked(_ sender: UIButton) {
+        print("BUTTON PRESSED")
+    }
+    
+    //Request Location Access
+    func requestLocationAccess() {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return
+            
+        case .denied, .restricted:
+            print("location access denied")
+            
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 
 }
+
+extension FirstViewController: MKMapViewDelegate {
+    
+}
+
 
